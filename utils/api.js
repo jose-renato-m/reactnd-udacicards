4dfc4e05e04cc3aa7688bb56d075957910207ee8
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-10 16:45:29 -0300

    feat: Create dummy variable

--- utils/api.js
+++ utils/api.js
@@ -7,5 +7,52 @@ const DECKS_STORAGE_KEY = 'MobileFlashcards: Decks'
  * Using an async data approach with dummy data. Could mature to REST API later.
  */
 export async function fetchAllDecks() {
-    
+
+}
+
+
+// --------------------------------------------------------------------------------
+
+
+const dummyData = {
+    CapitalCities: {
+        id: 'CapitalCities',
+        title: 'Capital Cities',
+        timestamp: 15637968800,
+        created: '2020-02-09',
+        questions: [
+            {
+                question: 'What is the capital of Canada?',
+                answer: 'Ottawa',
+            },
+            {
+                question: 'What is the capital of China?',
+                answer: 'Beijing',
+            },
+            {
+                question: 'What is the capital of Poland?',
+                answer: 'Warsaw',
+            },
+            {
+                question: 'What is the capital of Germany?',
+                answer: 'Berlin',
+            }
+        ]
+    },
+    React: {
+        id: 'React',
+        title: 'React',
+        timestamp: 1563710400,
+        created: '2020-02-08',
+        questions: [
+            {
+                question: 'What is React?',
+                answer: 'A library for managing user interfaces',
+            },
+            {
+                question: 'Where do you make Ajax requests in React?',
+                answer: 'The componentDidMount lifecycle event',
+            }
+        ]
+    }
 }

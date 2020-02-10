639907950ff84244070dc0c68f8ff330cd240d56
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-10 16:29:33 -0300

    feat: Export async function

--- utils/api.js
+++ utils/api.js
@@ -2,3 +2,10 @@ import { AsyncStorage } from 'react-native'
 
 const DECKS_STORAGE_KEY = 'MobileFlashcards: Decks'
 
+/**
+ * Get's all the decks of flashcards.
+ * Using an async data approach with dummy data. Could mature to REST API later.
+ */
+export async function fetchAllDecks() {
+    
+}

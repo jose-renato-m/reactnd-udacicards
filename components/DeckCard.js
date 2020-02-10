febd672b13af4c1c263265bf445fb8e30f050460
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-10 20:22:34 -0300

    feat: Import proptypes from library

--- components/DeckCard.js
+++ components/DeckCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { View, Text, StyleSheet, TouchableOpacity, Animated } from 'react-native'
 
 class DeckCard extends Component {

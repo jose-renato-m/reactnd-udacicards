6fb21999472fbe2e020dd5fff65683aba9d970c9
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 11:57:46 -0300

    feat: Import styles from utils

--- components/Question.js
+++ components/Question.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import globalStyles from '../utils/globalStyles'
+import { robotoMedium, robotoRegular } from '../utils/fonts'
+import { textColor, white } from '../utils/colors'
 
 
 class Question extends Component {

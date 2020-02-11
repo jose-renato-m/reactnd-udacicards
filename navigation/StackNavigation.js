33c3d3551a1da1fd1c19943cc28e714ada312336
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 19:48:27 -0300

    feat: Import applications from libs

--- navigation/StackNavigation.js
+++ navigation/StackNavigation.js
@@ -1,3 +1,8 @@
 import React from 'react'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+
+import TabNavigation from './TabNavigation'
+import Decks from '../views/Decks'
+
 
 export default StackNavigation

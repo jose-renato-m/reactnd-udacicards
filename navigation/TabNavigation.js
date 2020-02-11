6c5985b387b8afb535091173cfe2e21d588a6353
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 16:07:12 -0300

    feat: Import applications from libs

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,3 +1,10 @@
 import React from 'react'
+import { Platform } from 'react-native'
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { bgBlue, white } from '../utils/colors'
+import { robotoRegular } from '../utils/fonts'
+
+
 
 export default TabNavigation

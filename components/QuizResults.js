a179a2c8fdc9b80365c9c5d70641020f6a9ec263
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 15:34:11 -0300

    feat: Import applications from libs

--- components/QuizResults.js
+++ components/QuizResults.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import globalStyles from '../utils/globalStyles'
+import { robotoMedium } from '../utils/fonts'
+import { textColor } from '../utils/colors'
+import NavigationService from '../navigation/navigationService'
 
 function QuizResults (props) {
 

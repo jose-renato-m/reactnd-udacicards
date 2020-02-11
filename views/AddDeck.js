e7ee403e9867d092aabb8320f1d6bc9d602945fd
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 18:16:45 -0300

    feat: Import applications from libs

--- views/AddDeck.js
+++ views/AddDeck.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import globalStyles from '../utils/globalStyles'
+import { textColor } from '../utils/colors'
+import { robotoMedium } from '../utils/fonts'
+import CustomStatusBar from '../components/CustomStatusBar'
+import { addDeck } from '../actions/index'
 
 class AddDeck extends Component {
-
+    
 }
 
 export default connect()(AddDeck)

16e2334a541fe1c97f75e7012c12487a1626d52f
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 16:25:01 -0300

    feat: Import applications from libs

--- views/Decks.js
+++ views/Decks.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Text, View, ScrollView } from 'react-native'
+import globalStyles from '../utils/globalStyles'
+
+import CustomStatusBar from '../components/CustomStatusBar'
+import HomeHeader from '../components/HomeHeader'
+import DeckCard from '../components/DeckCard'
 
 class Decks extends Component {
 

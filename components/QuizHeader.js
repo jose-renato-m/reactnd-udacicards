61bb9e73f28a40b796fe94982864322cb6b244d2
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 15:13:06 -0300

    feat: Add styles objects

--- components/QuizHeader.js
+++ components/QuizHeader.js
@@ -7,6 +7,38 @@ import { robotoMedium } from '../utils/fonts'
 export default function QuizHeader(props) {
 
     return (
-
+        
     )
 }
+
+const styles = StyleSheet.create({
+    headerPanel: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingLeft: 16,
+        paddingRight: 16,
+        height: 130,
+        borderRadius: 10,
+        backgroundColor: bgBlueLight,
+    },
+    headerTextContainer: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    headerText: {
+        color: white,
+        fontFamily: robotoMedium,
+        fontSize: 32,
+    },
+    countText: {
+        marginTop: 24,
+        fontSize: 26,
+        fontFamily: robotoMedium,
+        color: white,
+    },
+    imageContainer: {
+        flex: 1,
+        alignItems: 'flex-end'
+    }
+})

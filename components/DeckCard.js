662113eba851a2c5aeaf587f37c7861b36a7a2e7
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 10:01:29 -0300

    feat: Import navigation stateless component

--- components/DeckCard.js
+++ components/DeckCard.js
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet, TouchableOpacity, Animated } from 'react-native
 import { FontAwesome } from '@expo/vector-icons'
 import { bgGreen, white } from '../utils/colors'
 import { robotoMedium, robotoRegular } from '../utils/fonts'
+import NavigationService from '../navigation/navigationService'
 
 class DeckCard extends Component {
 

7db118dd10b615cc513279187e7b027041456c42
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 15:02:55 -0300

    feat: Import applications from libs

--- components/QuizHeader.js
+++ components/QuizHeader.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { white, bgBlueLight } from '../utils/colors'
+import { robotoMedium } from '../utils/fonts'
 
 export default function QuizHeader(props) {
 

636bd877769817cab3308771936804641825ee04
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 11:38:49 -0300

    feat: Create globalstyles structure

---
+++ utils/globalStyles.js
@@ -0,0 +1,5 @@
+import { StyleSheet } from 'react-native'
+import { bgBlue, textColor, textRed, white } from './colors'
+import { robotoMedium, robotoRegular } from './fonts'
+
+export default globalStyles

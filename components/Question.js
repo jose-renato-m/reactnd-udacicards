8871170d41e9a3a271d9887cf2320c389c14d9a0
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 11:34:53 -0300

    feat: Import applications from libs

--- components/Question.js
+++ components/Question.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+
 
 class Question extends Component {
 

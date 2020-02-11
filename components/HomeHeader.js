24d01cd8f375258ad95ed8fa0a414b7b4f2cd9c8
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 11:22:06 -0300

    feat: Add styles objects

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -6,6 +6,34 @@ import { robotoMedium, robotoRegular } from '../utils/fonts'
 export default function HomeHeader() {
 
     return (
-        
+
     )
 }
+
+const styles = StyleSheet.create({
+    headerPanel: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 8,
+        height: 130,
+        borderRadius: 10,
+        backgroundColor: bgBlueLight,
+    },
+    headerTextContainer: {
+        flexDirection: 'column'
+    },
+    headerText: {
+        textAlign: 'center',
+        color: white,
+        fontFamily: robotoMedium,
+        fontSize: 32,
+    },
+    headerTagline: {
+        textAlign: 'center',
+        marginTop: 10,
+        color: white,
+        fontFamily: robotoRegular,
+        fontSize: 13,
+    },
+})

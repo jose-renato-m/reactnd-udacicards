858dbbb3edfacfe00d3f78704f2fcb3780e97ed3
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 11:13:42 -0300

    feat: Import applications from libraries

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { white, bgBlueLight } from '../utils/colors'
+import { robotoMedium, robotoRegular } from '../utils/fonts'
 
 export default function HomeHeader() {
-    
+
 }

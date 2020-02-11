d9a5a729d4111bac9ac4b4e8b671d8891494f06f
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-11 19:40:36 -0300

    feat: Create tabnavigator variable

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -8,7 +8,46 @@ import { robotoRegular } from '../utils/fonts'
 import Decks from '../views/Decks'
 import AddDeck from '../views/AddDeck'
 
-
+const TabNavigator = createBottomTabNavigator(
+    {
+        Decks: {
+            screen: Decks,
+            navigationOptions: {
+                tabBarLabel: 'Decks',
+                tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards-outline' size={30} color={tintColor} />
+            }
+        },
+        AddDeck: {
+           screen: AddDeck,
+           navigationOptions: {
+               tabBarLabel: 'Add Deck',
+               tabBarIcon: ({ tintColor }) => <MaterialIcons name='add' size={30} color={tintColor} /> 
+           } 
+        }
+    },
+    {
+        navigationOptions: {
+            header: null
+        },
+        tabBarOptions: {
+            activeTintColor: Platform.OS === 'ios' ? bgBlue : white,
+            style: {
+                height: 56,
+                backgroundColor: Platform.OS === 'ios' ? '#FAFAFA' : bgBlue,
+                fontFamily: robotoRegular,
+                shadowColor: 'rgba(0,0,0,0.24)',
+                shadowOffset: {
+                    width: 0,
+                    height: 3,
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1,
+            },
+            labelStyle: {
+                fontSize: 13,
+            },
+        },
+    })
 
 
 

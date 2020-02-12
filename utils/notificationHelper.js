0fbd7ab6eab4ed50afbbff990cad943c59db92c1
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-12 17:10:06 -0300

    feat: Export function

--- utils/notificationHelper.js
+++ utils/notificationHelper.js
@@ -1,9 +1,26 @@
 import { Notifications, Permissions } from 'expo'
 import { AsyncStorage } from 'react-native'
+import { func } from 'prop-types'
 
 const NOTIFICATION_STORAGE_KEY = 'MobileFlashcards:Notification'
 
 export async function clearLocalNotification() {
     await AsyncStorage.removeItem(NOTIFICATION_STORAGE_KEY)
     await Notifications.cancelAllScheduledNotificationsAsync()
+}
+
+export function createNotification() {
+    return {
+        title: 'Answer a quiz',
+        body: '⏰ Don\'t forget to complete a quiz today',
+        ios: {
+            sound: true
+        },
+        android: {
+            sound: true,
+            priority: 'high',
+            sticky: false,
+            vibrate: true,
+        }
+    }
 }

94278ba39b46d6c246fb78091a92137d6ba31d29
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-12 08:24:41 -0300

    feat: Import applications from libs

--- views/Deck.js
+++ views/Deck.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { View, Text, TouchableOpacity } from 'react-native'
+import globalStyles from '../utils/globalStyles'
+import NavigationService from '../navigation/navigationService'
+
+import DeckCard from '../components/DeckCard'
 
 class Deck extends Component {
 

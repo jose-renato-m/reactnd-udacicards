047539929b2d7035ef5564a4cff3f891d1cfed94
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-12 18:17:17 -0300

    feat: Import missing applications from libs

--- navigation/StackNavigation.js
+++ navigation/StackNavigation.js
@@ -3,6 +3,12 @@ import { createStackNavigator, createAppContainer } from 'react-navigation'
 
 import TabNavigation from './TabNavigation'
 import Decks from '../views/Decks'
+import Deck from '../views/Deck'
+import AddDeck from '../views/AddDeck'
+import AddCard from '../views/AddCard'
+import Quiz from '../views/Quiz'
+
+
 
 
 export default StackNavigation

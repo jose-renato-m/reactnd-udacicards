6643f20591062bbec2c063b5555204e07023a790
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-12 17:32:10 -0300

    feat: Import components

--- views/Quiz.js
+++ views/Quiz.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { View } from 'react-native'
 import globalStyles from '../utils/globalStyles'
+import { clearLocalNotification, setLocalNotification } from '../utils/notificationHelper'
+
+import CustomStatusBar from '../components/CustomStatusBar'
+import QuizHeader from '../components/QuizHeader'
+import Question from '../components/Question'
+import QuizResults from '../components/QuizResults'
 
 class Quiz extends Component {
 

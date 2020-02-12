5a35c6ba3cbc3d97dbefc9df0a37163edab4afc1
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-12 08:33:55 -0300

    feat: Add arrow functions with destructuring

--- views/Deck.js
+++ views/Deck.js
@@ -11,6 +11,31 @@ class Deck extends Component {
     state = {
         showNoQuestionsError: false
     }
+
+    handleStartQuiz = () => {
+
+        const { deck, questionsCount } = this.props
+
+        if (questionsCount === 0) {
+            this.setState({ showNoQuestionsError: true })
+        } else {
+            NavigationService.navigate('Quiz', {
+                deckId: deck.id
+            })
+        }
+    }
+
+    handleAddCard = () => {
+
+        const { deck } = this.props
+
+        this.setState({ showNoQuestionsError: false })
+
+        NavigationService.navigate('AddCard', {
+            deckId: deck.id
+        })
+        
+    }
 }
 
 export default connect()(Deck)

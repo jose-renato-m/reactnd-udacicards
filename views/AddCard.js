71064e0a79a018d5a7b93f708483444cc5c6208f
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-12 09:01:51 -0300

    feat: Import applications from libs

--- views/AddCard.js
+++ views/AddCard.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
+import { addCard } from '../actions/index'
 import { connect } from 'react-redux'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import globalStyles from '../utils/globalStyles'
+import { textColor } from '../utils/colors'
+import { robotoMedium } from '../utils/fonts'
 
 class AddCard extends Component {
 

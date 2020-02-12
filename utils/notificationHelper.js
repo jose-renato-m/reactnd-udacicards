125c6155fe3f7559a6e1f6a4c54342d0133adaa8
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-12 17:23:10 -0300

    feat: Export async function

--- utils/notificationHelper.js
+++ utils/notificationHelper.js
@@ -24,3 +24,28 @@ export function createNotification() {
         }
     }
 }
+
+export async function setLocalNotification() {
+    const json = await AsyncStorage.getItem(NOTIFICATION_STORAGE_KEY)
+    const data = JSON.parse(json)
+
+    if (data === null) {
+        const status = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+
+        if (status === 'granted') {
+            await Notifications.cancelAllScheduledNotificationsAsync()
+
+            const tommorrow = getTomorrowAt8pm()
+
+            Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                    time: tomorrow,
+                    repeat: 'day'
+                }
+            )
+
+            AsyncStorage.setItem(NOTIFICATION_STORAGE_KEY, JSON.stringify(true))
+        }
+    }
+}

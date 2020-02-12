1dcbfa163c4dd91a58d7a11f7f30758e3951b4d6
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-02-12 18:35:23 -0300

    feat: Import applications from libs

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers/index'
+import middleware from './middleware/index'
+import { receiveDecks } from './actions/index'
 
 export default function App() {
   return (
